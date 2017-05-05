45ddbfc6ced5e7faee0d5b072267dbd05ba96bc3
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2017-05-05 10:44:48 +0000

    おためし

--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -2,4 +2,21 @@ class Tracking < ApplicationRecord
   belongs_to :owner, class_name: User.to_s
   has_many :observations
   has_many :observers, class_name: User.to_s, through: :observations, foreign_key: :user_id
+  
+  before_validation :set_code
+  
+  private
+  
+  def set_code
+    return true if self.code.present?
+    
+    10.times do
+      new_code = SecureRandom.uuid
+      if not Tracking.where(code: new_code).exists?
+        self.code = new_code
+        return true
+      end
+    end  
+    false
+  end
 end
