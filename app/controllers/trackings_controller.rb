ba3dc5848d9b9798fa8d84ce24d4e09bedae1b4d
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2017-05-12 23:59:52 +0000

    リファクタリング

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,23 +1,24 @@
 require 'GcmPush'
 
 class TrackingsController < ApplicationController
+  before_action :set_tracking, only: [:show]
+  after_action :request_updating_location_log, only: [:show]
+
   def create
     user_params = params.require(:user).permit(:name, :gcm_token)
     raise BadRequestError if user_params[:gcm_token].blank?
 
-    user = User.find_or_initialize_by(user_params)
-    user.save!
+    ApplicationRecord.transaction do
+      user = User.find_or_initialize_by(user_params)
+      user.save!
+      @tracking = user.trackings.order(:updated_at).reverse_order.first_or_create!
+    end
 
-    tracking = user.trackings.order(:updated_at).reverse_order.first_or_create!
-
-    json_tracking = tracking.slice(:id_code)
+    json_tracking = @tracking.slice(:id_code)
     json_tracking[:id] = json_tracking.delete(:id_code)
     render json: json_tracking, status: :ok
   end
   
-  before_action :set_tracking, only: [:show]
-  after_action :request_updating_location_log, only: [:show]
-
   def show
     location_log = @tracking.location_logs.recent_enough_for_cached_view.last
     json_location = location_log.try(:slice, :id, :lat, :lon, :accuracy, :created_at)
